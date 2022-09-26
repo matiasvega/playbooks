3e9bc56350d8ee8601953ed6d4c3742359f12cc8
matiasvega <matiasvega@gmail.com>
2022-09-26 13:54:50 -0300

    add sql scripts

---
+++ sql/ts.sql
@@ -0,0 +1,14 @@
+PROMPT +-----------+
+PROMPT TABLESPACES 
+PROMPT +-----------+
+@ts_free_space.sql
+
+PROMPT +-----------+
+PROMPT DATAFILES 
+PROMPT +-----------+
+@datafiles.sql
+
+PROMPT +-----------+
+PROMPT ASM DISKGROUPS 
+PROMPT +-----------+
+@asm_diskgroups.sql
